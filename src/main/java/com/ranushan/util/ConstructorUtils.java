e5fab6ca2586f8c17b1e3c8d94cb039813a93964
Ranushan <ranusha.n@hotmail.fr>
2024-05-27 02:23:23 +0200

    remove apache common

--- src/main/java/com/ranushan/util/ConstructorUtils.java
+++ src/main/java/com/ranushan/util/ConstructorUtils.java
@@ -3,12 +3,45 @@ package com.ranushan.util;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
-import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.*;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ConstructorUtils {
 
-    public static <T> T invokeConstructor(Class<T> cls, Object... args) throws InvocationTargetException, NoSuchMethodException, IllegalAccessException, InstantiationException {
-        return org.apache.commons.lang3.reflect.ConstructorUtils.invokeConstructor(cls, args);
+    public static <T> T invokeConstructor(Class<T> cls) throws InvocationTargetException, NoSuchMethodException, IllegalAccessException, InstantiationException {
+        Constructor<T> ctor = getMatchingAccessibleConstructor(cls);
+        if (ctor == null) {
+            throw new NoSuchMethodException(
+                    "No such accessible constructor on object: " + cls.getName());
+        }
+        return ctor.newInstance();
+    }
+
+    private static <T> Constructor<T> getMatchingAccessibleConstructor(Class<T> cls) throws NoSuchMethodException {
+        return setAccessibleWorkaround(cls.getConstructor());
+    }
+
+    private static <T extends AccessibleObject> T setAccessibleWorkaround(T obj) {
+        if (obj == null || obj.isAccessible()) {
+            return obj;
+        }
+        final Member m = (Member) obj;
+        if (!obj.isAccessible() && isPublic(m) && isPackageAccess(m.getDeclaringClass().getModifiers())) {
+            try {
+                obj.setAccessible(true);
+                return obj;
+            } catch (final SecurityException ignored) {
+                // ignore in favor of subsequent IllegalAccessException
+            }
+        }
+        return obj;
+    }
+
+    private static boolean isPublic(Member member) {
+        return member != null && Modifier.isPublic(member.getModifiers());
+    }
+
+    private static boolean isPackageAccess(int modifiers) {
+        return (modifiers & (Modifier.PUBLIC | Modifier.PROTECTED | Modifier.PRIVATE)) == 0;
     }
 }
